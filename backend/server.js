7d9f6ef7b433f0e34cf25401714fd61992e86e1c
Danielle Barker <caseybarker86@gmail.com>
2024-11-06 13:28:33 -0700

    added create todo route

--- backend/server.js
+++ backend/server.js
@@ -6,16 +6,17 @@ const PORT = 7777;
 const app = express();
 app.use(express.json());
 
-app.get("/", async (req, res) => {
-  const todos = await getTodos();
-  res.send(todos);
-});
+app.get("/", getTodosRoute);
+app.post("/", createTodoRoute);
 
 app.listen(PORT, () => console.log("listening on ", PORT));
 
-// async function createTodoRoute(req, res) {
-//   const newTodo = req.body;
-//   const created = await createTodo(newTodo);
-//   res.send(created);
-// }
-// app.post("/", createTodoRoute);
+async function getTodosRoute(req, res) {
+  const todos = await getTodos();
+  res.send(todos);
+}
+async function createTodoRoute(req, res) {
+  const newTodo = req.body;
+  const created = await createTodo(newTodo);
+  res.send(created);
+}
