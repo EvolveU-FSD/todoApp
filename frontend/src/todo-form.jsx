b74963464dc221b473f4bd31b6e0280e9f3899e1
Danielle Barker <caseybarker86@gmail.com>
2024-11-06 14:19:26 -0700

    create the todoForm and installed axios

--- frontend/src/todo-form.jsx
+++ frontend/src/todo-form.jsx
@@ -1,5 +1,30 @@
+import { useState } from "react";
+import axios from "axios";
 const TodoForm = () => {
-  return <div>TodoForm</div>;
+  const [title, setTitle] = useState();
+  const handleChange = (e) => {
+    const newValue = e.target.value;
+    setTitle(newValue);
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // send post request to "/api" with the task in the body as the title
+    const response = await axios.post("/api", { title });
+    const data = response.data;
+    console.log("data", data);
+  };
+  return (
+    <main>
+      <form>
+        <input
+          onChange={handleChange}
+          value={title}
+          className="todo-form-input"
+        />
+        <button onClick={handleSubmit}>Add Todo</button>
+      </form>
+    </main>
+  );
 };
 
 export default TodoForm;
